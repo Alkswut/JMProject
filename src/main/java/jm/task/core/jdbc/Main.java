1c083cdd5c472d2098eacf82c6b92e70054fa7cb
AleksandrVoroninSME <aleksandr_av@bk.ru>
2022-01-01 23:53:53 +0300

    Task_1_1_3

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
+
         UserService userService = new UserServiceImpl();
         userService.createUsersTable();
 
